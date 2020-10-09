b2b9153069d1c9b06daa548c08a51b2c5378cfba
xk <permission@hnu.edu.cn>
2020-10-09 20:40:35 +0800

    new run sh script

--- run_ndn.sh
+++ run_ndn.sh
@@ -6,7 +6,8 @@
 TRACE_DIR=`pwd`"/data"
 SPEED=$1
 PKT_SIZE=$2
+TP_MAP=0x1
 
 echo "Test Packt Size=$PKT_SIZE Bytes"
 
-./build/pkt-sender -c 0x1ff -n 4 -w 3b:00.0 -w 3b:00.1 -- -t $TRACE_DIR/name.csv -s $SPEED -L $PKT_SIZE -P 0x1
+./build/pkt-sender -c 0x1ff -n 4 -w 3b:00.0 -w 3b:00.1 -- -t $TRACE_DIR/name.csv -s $SPEED -L $PKT_SIZE -P $TP_MAP
