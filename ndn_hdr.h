8347b23af6e95f98a5c903de5e205d09a139a1a7
xk <permission@hnu.edu.cn>
2020-10-10 10:29:02 +0800

    new ndn header and rss swtich macro

---
+++ ndn_hdr.h
@@ -0,0 +1,21 @@
+#ifndef NDN_H
+#define NDN_H
+
+#include <unistd.h>
+#include <rte_ether.h>
+
+#include "common.h"
+
+struct __attribute__((aligned)) ndn_hdr{
+  struct rte_ether_hdr ether;
+
+#ifdef RSS_ON
+  struct rte_ipv4_hdr ipv4;
+  struct rte_tcp_hdr tcp;
+#endif
+
+  uint32_t name_len;
+  uint8_t name[256];
+};
+
+#endif
