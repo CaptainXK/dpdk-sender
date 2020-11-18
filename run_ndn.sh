84235309028ea1790f91dab45303f8d3107676d5
xk <permission@hnu.edu.cn>
2020-11-18 10:42:55 +0800

    new run sh

--- run_ndn.sh
+++ run_ndn.sh
@@ -4,10 +4,12 @@
 #s speed Mbps
 #L pkt length
 TRACE_DIR=`pwd`"/data"
+# TRACE_FILE=test.csv
+TRACE_FILE=traces.csv
 SPEED=$1
 PKT_SIZE=$2
 TP_MAP=0x1
 
 echo "Test Packt Size=$PKT_SIZE Bytes"
 
-./build/pkt-sender -c 0x1ff -n 4 -w 3b:00.0 -w 3b:00.1 -- -t $TRACE_DIR/name.csv -s $SPEED -L $PKT_SIZE -P $TP_MAP
+./build/pkt-sender -c 0x1fff -n 4 -w 3b:00.0 -w 3b:00.1 -- -t ${TRACE_DIR}/${TRACE_FILE} -s $SPEED -L $PKT_SIZE -P $TP_MAP
